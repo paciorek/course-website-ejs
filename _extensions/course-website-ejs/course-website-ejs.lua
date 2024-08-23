812ba80e51e453d03c90b70f8a2e2dde812b325e
andrewpbray <andrewbray@berkeley.edu>
2024-08-23 16:06:37 -0700

    insert into ast meta instead

--- _extensions/course-website-ejs/course-website-ejs.lua
+++ _extensions/course-website-ejs/course-website-ejs.lua
@@ -1,11 +1,32 @@
 
 function Meta(meta) 
   
-  local project_profile = os.getenv("QUARTO_PROFILE")
+  --local project_profile = os.getenv("QUARTO_PROFILE")
+  local project_profile = "staff-site"
+  
   -- inject project profile into html <meta> tag for use by ejs template
   quarto.doc.include_text("in-header", "<meta project-profile='" ..
     tostring(project_profile) .. "'>")
   
   quarto.doc.include_text("in-header", "<script>const projectProfile = document.querySelector('meta[project-profile]')?.getAttribute('project-profile');</script>")
+   
+   -- Create an Inlines object with a single Str element
+  
+  if project_profile ~= nil then
+    
+    local str_element = pandoc.Str(project_profile)
+    local inlines = pandoc.Inlines(str_element)
+    
+    -- Create the nested metadata structure
+    meta.listing = pandoc.MetaMap({
+      template_params = pandoc.MetaMap({
+        projectProfile = pandoc.MetaInlines(inlines)
+      })
+    })
+  
+    quarto.log.output(meta)
+    
+  end
+  
 
 end
