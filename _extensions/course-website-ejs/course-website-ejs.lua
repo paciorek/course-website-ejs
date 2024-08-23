7d64e83c32a4bdf5809f58069248bdd4ee3fef8d
andrewpbray <andrewbray@berkeley.edu>
2024-08-23 13:53:45 -0700

    inject script as well

--- _extensions/course-website-ejs/course-website-ejs.lua
+++ _extensions/course-website-ejs/course-website-ejs.lua
@@ -6,4 +6,6 @@ function Meta(meta)
   quarto.doc.include_text("in-header", "<meta project-profile='" ..
     tostring(project_profile) .. "'>")
   
+  quarto.doc.include_text("in-header", "<script>const projectProfile = document.querySelector('meta[project-profile]')?.getAttribute('project-profile');</script>")
+
 end
